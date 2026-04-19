11d4c983d0e3d57d2a477f237d9b186e8ee31b64
Sanjith Saha <it24044@mbstu.ac.bd>
2026-04-20 01:52:18 +0600

    Add Main class to create and display Car info

---
+++ Day-1(19-04-2026)/Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[] args){
+  Car myCar=new Car();
+  myCar.color="White";
+  myCar.model="Audi";
+  myCar.year=2025;
+  myCar.displayInfo();
+    }
+}
