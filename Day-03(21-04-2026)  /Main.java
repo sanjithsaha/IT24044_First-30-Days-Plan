6b2314be1f54d77c472070431a18b51c974fcd47
Sanjith Saha <it24044@mbstu.ac.bd>
2026-04-21 22:27:22 +0600

    Create Main.java

---
+++ Day-03(21-04-2026)  /Main.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String[] args){
+        Teacher t1=new Teacher();
+        t1.setInformation("Anisul Islam","Male","1749272722");
+        t1.show();
+       Teacher t2=new Teacher();
+        t2.setInformation("mahmud","Male","017797273923");
+        t2.show();
+    }
+}
